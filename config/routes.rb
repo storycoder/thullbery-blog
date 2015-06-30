ab59d005f388f4ae4a17b8f33d34347b88689a37
storycoder <joseph.margolis@gmail.com>
2015-06-30 18:16:16 -0400

    install devise and generate a devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   resources :blog_posts
   # The priority is based upon order of creation: first created -> highest priority.
