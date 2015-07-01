da2463b0a3fa1cff6667f6725fc329d0da385927
storycoder <joseph.margolis@gmail.com>
2015-07-01 12:07:13 -0400

    associate blogposts and comments to users and their respective usernames

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630222937) do
+ActiveRecord::Schema.define(version: 20150701152506) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150630222937) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
